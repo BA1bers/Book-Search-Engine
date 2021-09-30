62008d8df2ba937e30d5181942d1b195e496a43b
BA1bers <brent.albers@yahoo.com>
2021-09-29 20:01:27 -0700

    testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,6 +8,3 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
 });
 
 module.exports = mongoose.connection;
-
-//mongodb+srv://ba1bers:h4mb0rg3r@transaction.rcce9.mongodb.net/googlebooks?retryWrites=true&w=majority
-mongodb+srv://ba1bers:h4mb0rg3r@transaction.rcce9.mongodb.net/librarian?retryWrites=true&w=majority
